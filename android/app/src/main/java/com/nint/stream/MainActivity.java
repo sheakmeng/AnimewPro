b642f040d65b9fdf06bd964a2e55458110d5623f
sheakmeng2012-tech <sheakmeng2012@gmail.com>
2026-08-29 16:53:40 +0700

    Fix Android system navigation bar overlapping video controls

--- android/app/src/main/java/com/nint/stream/MainActivity.java
+++ android/app/src/main/java/com/nint/stream/MainActivity.java
@@ -1,5 +1,16 @@
 package com.nint.stream;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Ensure webview and video player controls are never covered by system navigation bar (||| O <)
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+    }
+}
